0c7a26a8c4779944dc6b446169c26aa8caefc4f7
Vivian Lim <vivlim@pm.me>
2020-08-31 21:23:06 -0700

    Patch config.toml with SITE_URI env variable

--- on_request.sh
+++ on_request.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "GIT_URI=$GIT_URI"
 echo "GIT_BRANCH=$GIT_BRANCH"
+[ ! -z "$SITE_URI" ] && echo "SITE_URI=$SITE_URI"
 
 [ ! -d "/home/repo" ] && cd /home && git clone -b $GIT_BRANCH $GIT_URI repo
 
@@ -12,4 +13,8 @@ git fetch --all
 git fetch --prune
 git reset --hard origin/$GIT_BRANCH
 git submodule init && git submodule update
+
+# patch config if SITE_URI is set
+[ ! -z "$SITE_URI" ] && sed -i 's@^base_url.*@base_url = '"$SITE_URI"'@' ./config.toml
+
 /bin/zola check && /bin/zola build --output-dir /home/out/site
